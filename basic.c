648af285651e04333d85fd7a5d531a7bff46e9ae
Ysrael Abilon <ysrael.abilon2k4@gmail.com>
2025-10-29 20:34:44 +0800

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    
+    printf("===============================\n");
+    printf("\t POS SYSTEM ROLE SELECT\n");
+    printf("===============================\n");
+
+
+    return 0;
+
+    
+   }
